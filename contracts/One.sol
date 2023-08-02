0659e10b87dfc61e79ffd4d28335afc580f0178e
Revathi2604 <revathi26042002@gmail.com>
2023-08-02 15:46:51 +0530

    upload code

---
+++ contracts/One.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+contract One {
+    uint  public num1 = 0;
+    int public num = -2;
+    bool public value = true;
+    bytes public name = "Revathi";
+    string public name2 = "S";
+    address public owner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+}
